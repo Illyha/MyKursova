f6da9214a8a0f469704d36b56527ee9ae6458de4
Illyha <Illyha@Illyha.local>
2019-11-19 14:28:49 +0200

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => (
+  
+   <footer className="footer">
+      <div className="footer__copyright">
+      &copy; Made with ❤️ by <a className="footer__copyright--link" href="">Illya Slivchuk in 2019</a></div>
+   </footer>
+);
+
+export default footer; 
